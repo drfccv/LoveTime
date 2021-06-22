2789ec06c04a119103860eeb1801ee1d18c76302
drfccv <65041949+drfccv@users.noreply.github.com>
2021-06-23 05:29:51 +0800

    Update lovetime.js

--- js/lovetime.js
+++ js/lovetime.js
@@ -12,7 +12,7 @@ function lovetime() {
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    // 修改这里时间即可 我的是2018.11.19
+    // 修改这里时间即可 我的是2021.05.04
     var t1 = Date.UTC(2021, 05, 04, 00, 00, 00)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
